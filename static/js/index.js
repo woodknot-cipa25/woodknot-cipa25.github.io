93366dc603d447cf7a831d32bd1686ddc5ba60f1
J.Q. Pan <81284967+happy-panda-ops@users.noreply.github.com>
2025-07-08 11:34:09 +0200

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,16 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
+document.addEventListener('DOMContentLoaded', () => {
+  bulmaCarousel.attach('.results-carousel', {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    loop: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnHover: true,
+    navigation: true,
+    pagination: true,
+  });
 
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
-
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
-    }
-
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-	
-    bulmaSlider.attach();
-
-})
+  bulmaSlider.attach();
+});
